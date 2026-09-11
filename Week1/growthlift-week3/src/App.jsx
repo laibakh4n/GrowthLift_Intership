0d45a95c7b06369a6c136bff3a5a8eede4476a41
laibakh4n <laybakhn6@email.com>
2026-09-11 17:04:41 +0500

    Week3_day2 added

--- Week1/growthlift-week3/src/App.jsx
+++ Week1/growthlift-week3/src/App.jsx
@@ -1,7 +1,19 @@
 import Header from "./components/Header";
+import Card from "./components/Card";
+import Counter from "./components/Counter";
+import Toggle from "./components/Toggle";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Card title="React" description="JS library for UIs" color="#A78BFA" />
+      <Card title="Vite" description="Fast build tool" color="#00C864" />
+      <Card title="Tailwind" description="Utility-first CSS" color="#00B4D8" />
+      <Counter />
+      <Toggle />
+    </>
+  );
 }
 
 export default App;
